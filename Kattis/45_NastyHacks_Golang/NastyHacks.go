b42e88627b941237216c930bf113bd0caf25871f
Dean <dean.s.coakley@gmail.com>
2017-10-03 10:26:11 +0100

    Add basic Go solutions

---
+++ Kattis/45_NastyHacks_Golang/NastyHacks.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var tests, noAds, ads, cost int
+	fmt.Scanln(&tests)
+
+	for i := 0; i < tests; i++ {
+		fmt.Scanln(&noAds, &ads, &cost)
+
+		if noAds < ads - cost {
+			fmt.Println("advertise")
+		} else if noAds > ads - cost {
+			fmt.Println("do not advertise")
+		} else {
+			fmt.Println("does not matter")
+		}
+	}
+}
