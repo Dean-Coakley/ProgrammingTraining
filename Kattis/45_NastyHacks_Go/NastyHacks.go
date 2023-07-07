143f7d80845b2bcc38e491ffba1384846b090d49
Dean Coakley <dean.s.coakley@gmail.com>
2023-07-07 11:00:34 +0100

    Gofmt run

--- Kattis/45_NastyHacks_Go/NastyHacks.go
+++ Kattis/45_NastyHacks_Go/NastyHacks.go
@@ -9,9 +9,9 @@ func main() {
 	for i := 0; i < tests; i++ {
 		fmt.Scanln(&noAds, &ads, &cost)
 
-		if noAds < ads - cost {
+		if noAds < ads-cost {
 			fmt.Println("advertise")
-		} else if noAds > ads - cost {
+		} else if noAds > ads-cost {
 			fmt.Println("do not advertise")
 		} else {
 			fmt.Println("does not matter")
--- Kattis/46_Karte_Go/Karte.go
+++ Kattis/46_Karte_Go/Karte.go
@@ -3,50 +3,50 @@ package main
 import "fmt"
 
 func main() {
-  var input string
-  fmt.Scanln(&input)
+	var input string
+	fmt.Scanln(&input)
 
-  cards := [4]string {
-    input[0:3],
-    input[3:6],
-    input[6:9],
-    input[9:12],
-  }
+	cards := [4]string{
+		input[0:3],
+		input[3:6],
+		input[6:9],
+		input[9:12],
+	}
 
-  duplicateFound := false
+	duplicateFound := false
 
-  for i, card := range cards {
-    for j, searchCard := range cards {
-      if card == searchCard && i != j {
-        duplicateFound = true
-      }
-    }
-    if (duplicateFound) {
-      fmt.Printf("GRESKA\n")
-      break;
-    }
-  }
+	for i, card := range cards {
+		for j, searchCard := range cards {
+			if card == searchCard && i != j {
+				duplicateFound = true
+			}
+		}
+		if duplicateFound {
+			fmt.Printf("GRESKA\n")
+			break
+		}
+	}
 
-  if !duplicateFound {
-    numP, numK, numH, numT := 13, 13, 13, 13
+	if !duplicateFound {
+		numP, numK, numH, numT := 13, 13, 13, 13
 
-    for _, card := range cards{
-      switch string(card[0]) {
-        case "P":
-          numP--;
-          break;
-        case "K":
-          numK--;
-          break;
-        case "H":
-          numH--;
-          break;
-        case "T":
-          numT--;
-          break;
-        }
-    }
+		for _, card := range cards {
+			switch string(card[0]) {
+			case "P":
+				numP--
+				break
+			case "K":
+				numK--
+				break
+			case "H":
+				numH--
+				break
+			case "T":
+				numT--
+				break
+			}
+		}
 
-    fmt.Printf("%d %d %d %d\n", numP, numK, numH, numT)
-  }
+		fmt.Printf("%d %d %d %d\n", numP, numK, numH, numT)
+	}
 }
