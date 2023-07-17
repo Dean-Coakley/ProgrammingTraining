463ecf9c12da7f33a0a1d135f768da7b80d54a22
Dean Coakley <dean.s.coakley@gmail.com>
2023-07-17 18:51:48 +0100

    Add HackerRank solutions

---
+++ HackerRank/01_TMP_Python3/odd_even.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n % 2 != 0 or n % 2 == 0 and n >= 6 and n<=20:
+        print("Weird")
+    elif n >= 2 and n <= 5 or n>20:
+        print("Not Weird")
+        
